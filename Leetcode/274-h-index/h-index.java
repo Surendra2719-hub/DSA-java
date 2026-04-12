6da7510ba8fd49c35691ad2f601b73fd85b618da
Surendra2719-hub <basurendra6@gmail.com>
2026-04-12 16:11:46 +0530

    Time: 5 ms (68.80%) | Memory: 43.4 MB (63.91%) - LeetSync

--- Leetcode/274-h-index/h-index.java
+++ Leetcode/274-h-index/h-index.java
@@ -1,37 +1,10 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int n=citations.length;
-        // for(int j=n; j>=0; j--){
-        //     int count=0;
-        //     for(int i=0; i<n; i++){
-        //         if(citations[i]>=j) count++;
-        //     }
-        //     if(count>=j){
-        //         return j;
-        //     }
-            
-        // }
-        // return 0;
-     int i=0;
-        int j=n;
-        int ans=0;
-        while(i<=j){
-            int mid = i + (j-i)/2;
-            int count=0;
-            for(int k=0; k<n;k++){
-                if(citations[k]>=mid){
-                    count++;
-                }
-            }
-            if(count>=mid){
-                ans=mid;
-                i=mid+1;
-            }
-            else{
-                j=mid-1;
-            }
+        int n = citations.length;
+        Arrays.sort(citations);
+        for(int i=0; i<n; i++){
+            if(citations[i]>=n-i) return n-i;
         }
-        return ans;
-
+        return 0;
     }
 }
