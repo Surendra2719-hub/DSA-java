d0914319c78c24fa3fe001489780400fd864ff2e
Surendra2719-hub <basurendra6@gmail.com>
2026-04-14 17:09:20 +0530

    Time: 4 ms (48.32%) | Memory: 44.4 MB (59.35%) - LeetSync

--- Leetcode/125-valid-palindrome/valid-palindrome.java
+++ Leetcode/125-valid-palindrome/valid-palindrome.java
@@ -3,9 +3,10 @@ class Solution {
         String s1 = s.toLowerCase();
         StringBuilder sb = new StringBuilder();
 
-        for (char c : s1.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                sb.append(c);
+        for (int i=0; i<s1.length(); i++) {
+            if(s1.charAt(i)<='z' && s1.charAt(i)>='a' || s1.charAt(i)>='0' && s1.charAt(i)<='9'){
+            char c = s1.charAt(i);
+            sb.append(c);
             }
         }
 
