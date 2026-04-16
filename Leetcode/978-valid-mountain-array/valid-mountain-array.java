299b4f79c21f92770661ef755448f075022755d0
Surendra2719-hub <basurendra6@gmail.com>
2026-04-16 16:53:07 +0530

    Time: 2 ms (86.55%) | Memory: 47.4 MB (61.27%) - LeetSync

--- Leetcode/978-valid-mountain-array/valid-mountain-array.java
+++ Leetcode/978-valid-mountain-array/valid-mountain-array.java
@@ -4,14 +4,11 @@ class Solution {
         if(n<3) return false;
 
         int i=0;
-        while(i < n-1 && arr[i] < arr[i+1]){
+        while(i<n-1 && arr[i]<arr[i+1]){
             i++;
         }
-
-        if(i == 0 || i == n-1) return false;
-
- 
-        while(i < n-1 && arr[i] > arr[i+1]){
+        if(i==0 || i==n-1) return false;
+        while(i<n-1 && arr[i]>arr[i+1]){
             i++;
         }
         return i == n-1;   
