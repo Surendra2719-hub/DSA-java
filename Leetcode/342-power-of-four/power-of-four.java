58732fa300a69df9d473b3fe3e96cb181b701c51
Surendra2719-hub <basurendra6@gmail.com>
2026-04-21 22:10:42 +0530

    Time: 1 ms (91.27%) | Memory: 42.4 MB (86.82%) - LeetSync

--- Leetcode/342-power-of-four/power-of-four.java
+++ Leetcode/342-power-of-four/power-of-four.java
@@ -3,7 +3,6 @@ class Solution {
         if (n <= 0) return false;
         while (n % 4 == 0) {
             n = n / 4;
-            System.out.println(n);
         }
         return n==1;   
     }
