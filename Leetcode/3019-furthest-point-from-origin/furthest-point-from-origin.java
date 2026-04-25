1f9d8426bb680caba147e87ec846c46d3d5dbd06
Surendra2719-hub <basurendra6@gmail.com>
2026-04-25 14:05:57 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (46.01%) - LeetSync

--- Leetcode/3019-furthest-point-from-origin/furthest-point-from-origin.java
+++ Leetcode/3019-furthest-point-from-origin/furthest-point-from-origin.java
@@ -1,24 +1,21 @@
 class Solution {
     public int furthestDistanceFromOrigin(String moves) {
         char charArray[] = moves.toCharArray();
-        int l=0;
-        int r=0;
-        int d=0;
+        int l=0, r=0, d=0;
+        int ans = 0;
         for(int i=0; i<charArray.length;i++){
             if(charArray[i]=='L') l++;
             else if(charArray[i]=='R') r++;
             else d++;
         }
         if(l>=r){ 
-            int ans= l-r+d;
-            if(ans<0) return ans * (-1);
-            return ans;
+            ans= l-r+d;
+            if(ans<0) ans=ans*(-1);
         }
        else if(l<=r){
-            int ans= r-l+d;
-            if(ans<0) return ans * (-1);
-            return ans;
+            ans=r-l+d;
+            if(ans<0) ans=ans*(-1);
         }
-        return 0;
+        return ans;
     }
 }
