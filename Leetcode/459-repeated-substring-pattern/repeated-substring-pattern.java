bbb23e02798b0fcddd4ad75f567a07c44ecbb8b1
Surendra2719-hub <basurendra6@gmail.com>
2026-04-27 16:24:03 +0530

    Time: 77 ms (18.55%) | Memory: 46.8 MB (54.72%) - LeetSync

--- Leetcode/459-repeated-substring-pattern/repeated-substring-pattern.java
+++ Leetcode/459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         String str = s+s;
-        return (str.substring(1, str.length()-1)).contains(s);
+        String str1=(str.substring(1, str.length()-1));
+        return str1.contains(s);
     }
 }
