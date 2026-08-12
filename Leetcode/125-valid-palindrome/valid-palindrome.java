49078120b018ac8cb1079e978c7c08cc9098043a
Surendra2719-hub <basurendra6@gmail.com>
2026-08-12 15:31:07 +0530

    Time: 4 ms (45.51%) | Memory: 44.5 MB (48.24%) - LeetSync

--- Leetcode/125-valid-palindrome/valid-palindrome.java
+++ Leetcode/125-valid-palindrome/valid-palindrome.java
@@ -3,21 +3,20 @@ class Solution {
         String s1 = s.toLowerCase();
         StringBuilder sb = new StringBuilder();
 
-        for (int i=0; i<s1.length(); i++) {
-            if(s1.charAt(i)<='z' && s1.charAt(i)>='a' || s1.charAt(i)>='0' && s1.charAt(i)<='9'){
-            char c = s1.charAt(i);
-            sb.append(c);
-            }
+        for(int i=0; i<s1.length(); i++){
+           if(s1.charAt(i)>='a' && s1.charAt(i)<='z' || s1.charAt(i)>='0' && s1.charAt(i)<='9'){
+            sb.append(s1.charAt(i));
+           }
+            
         }
 
-        int i = 0;
-        int j = sb.length() - 1;
-        while (i < j) {
-            if (sb.charAt(i) != sb.charAt(j)) {
+        int i = 0, j=sb.length()-1;
+        while(i<=j){
+            if(sb.charAt(i)!=sb.charAt(j)){
                 return false;
             }
-            i++;
-            j--;
+            i++; j--;
+
         }
         return true;
     }
