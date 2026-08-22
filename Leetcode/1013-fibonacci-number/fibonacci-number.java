b0177edb81a66135a9a15b536db3fb14caa488d3
Surendra2719-hub <basurendra6@gmail.com>
2026-08-22 15:24:41 +0530

    Time: 9 ms (42.86%) | Memory: 42.1 MB (52.65%) - LeetSync

--- Leetcode/1013-fibonacci-number/fibonacci-number.java
+++ Leetcode/1013-fibonacci-number/fibonacci-number.java
@@ -1,11 +1,7 @@
 class Solution {
     public int fib(int n) {
-        //base case
         if(n<=1) return n;
-
-        //resursive case
         return fib(n-1)+fib(n-2);
-        
-    }
 
+    }
 }
